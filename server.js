8707e542aeb11f3c515baf0b87199284d65a27f5
kaikai6901 <huyhoangbk64@gmail.com>
2023-07-07 04:22:28 +0700

    abc

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const summaryRoutes = require('./src/routes/summaryRoutes');
 const newsRoutes = require('./src/routes/newsRoutes');
 const projectRoutes = require('./src/routes/projectRoutes');
 
+
+
 const db = require('./src/config/db');
 
 // Connect to db
@@ -11,6 +13,13 @@ db.connect();
 const app = express();
 const PORT = 5555;
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 // Middleware
 app.use(bodyParser.json());
 
